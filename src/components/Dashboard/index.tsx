1ad0686b2e0b0f01edc32455d672f35c0f2f304d
Alexandre <alexandresilva58@gmail.com>
2021-11-07 17:41:01 -0300

    feat: criado cards de summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles";
+
+
+export function Dashbord(){
+    return (
+        <>
+            <Container>
+                <Summary/>
+            </Container>
+        
+        </>
+    )
+}
