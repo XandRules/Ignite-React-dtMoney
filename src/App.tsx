6b8e3d7b287c7ac9682fc0301c665f4b09b4f2b1
Alexandre <alexandresilva58@gmail.com>
2021-11-08 20:06:02 -0300

    feat: criado component Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import { Dashbord } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionOpen,setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashbord/>
+
+      <Modal 
+                isOpen={isNewTransactionOpen} 
+                onRequestClose={handleCloseNewTransactionModal}
+                >
+                    <h2>
+                        Cadastrar Transação
+                    </h2>
+                </Modal>
+                
       <GlobalStyle />
     </>
   );
